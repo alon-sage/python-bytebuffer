898dab1782ac656c4578109543c11780a064f3df
Ivan Babintsev <ibabintsev@npoprogress.com>
2016-09-22 15:16:18 +0300

    added license and setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+from distutils.core import setup
+
+setup(
+    name='bytebuffer',
+    version='0.1.0',
+    author='Ivan Babintsev',
+    author_email='alon.sage@gmail.com',
+    license='Apache License 2.0',
+    url='https://github.com/alon-sage/python-bytebuffer',
+    description='High performance library for reading and writing values of primitive types. '
+                'Primitive values are translated to (or from) sequences of bytes.',
+
+    packages=[],
+    py_modules=['bytebuffer'],
+)
